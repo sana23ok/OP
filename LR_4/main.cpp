24c909ae8c52b2533942f595e395bf6d92203d6b
Oksana <116382404+sana23ok@users.noreply.github.com>
2023-04-13 22:06:24 +0300

    Add files via upload

---
+++ LR_4/main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<conio.h>
+#include"lib.h"
+using namespace std;
+
+int main(){
+    do {
+        int size = 3;
+        Date *arr = new Date[size];
+        enterDate(arr);
+        displayInput(arr);
+        handleD1andD2(arr);
+        handleD3(arr);
+        delete[] arr;
+        string message = " Press ENTER to run again, ESC to exit...";
+        cout<<endl<<message<<endl;
+    }while(_getch()==13);
+    return 0;
+}
+
---
+++ LR_4/validation.h
@@ -0,0 +1,4 @@
+#pragma once
+bool isLeap(int year);
+bool validateDate(int, int, int);
+void validateInput( int&, int&, int&, bool&);
