6bbd036bde30444ba391751afbfa7e34fa2bd1a0
Oksana <116382404+sana23ok@users.noreply.github.com>
2023-04-21 02:31:17 +0300

    README file is added

--- LR_4/validation.h
+++ LR_4/validation.h
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
+
 #pragma once
 bool isLeap(int year);
 bool validateDate(int, int, int);
 void validateInput( int&, int&, int&, bool&);
-=======
-#pragma once
-bool isLeap(int year);
-bool validateDate(int, int, int);
-void validateInput( int&, int&, int&, bool&);
->>>>>>> fd635a8 (new conmit)
